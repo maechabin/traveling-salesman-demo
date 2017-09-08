a4c32c5fe12f4ceb98cb2165874a992dbcf79de8
maechabin <mail@chab.in>
2017-09-08 15:07:44 +0900

    開発に必要なファイルを準備

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import { configureStore } from './store';
+import { demoReducer } from './demo/demoReducer';
+import { demoState } from './demo/demoState';
+
 import './index.css';
-import App from './App';
+import DemoContainer from './demo/DemoContainer';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore(demoReducer, demoState);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <DemoContainer />
+  </Provider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
