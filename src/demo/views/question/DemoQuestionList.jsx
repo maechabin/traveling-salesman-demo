5a2baa80eb559f2f157cab2edfc5aadfa7fd6ec4
maechabin <mail@chab.in>
2017-09-11 12:58:03 +0900

    経路リストを追加

---
+++ src/demo/views/question/DemoQuestionList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const DemoQuestionList = () => {
+  return (
+    <div>
+      <ul>
+        <li>出発地: 根津神社</li>
+      </ul>
+      <ul>
+        <li>経路A: 秋葉原駅</li>
+        <li>経路B: 文京区役所</li>
+        <li>経路C: 明治大学</li>
+        <li>経路D: 浜離宮</li>
+        <li>経路E: 国会議事堂</li>
+      </ul>
+      <ul>
+        <li>到着地: 勝どき駅</li>
+      </ul>
+    </div>
+  );
+};
+
+export default DemoQuestionList;
