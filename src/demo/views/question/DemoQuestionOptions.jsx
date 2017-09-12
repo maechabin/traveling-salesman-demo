4ac0c49a9577c910bcdabe4af625726a44bbfeaf
maechabin <mail@chab.in>
2017-09-13 01:28:45 +0900

    リファクタリングなど

--- src/demo/views/question/DemoQuestionOptions.jsx
+++ src/demo/views/question/DemoQuestionOptions.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const DemoQuestionOptions = (props) => {
   const { transport, expressway, traffic, handleFormChange } = props;
@@ -11,36 +12,37 @@ const DemoQuestionOptions = (props) => {
   }
 
   return (
-    <dl>
+    <dl className="DemoQuestionOptions">
       <dt>移動手段</dt>
       <dd>
-        <inputLabel>
-          車: <input type="radio" value="car" name="transport" checked={transport === 'car'} onChange={handleChange} />
-        </inputLabel>
-        <inputLabel>
-          徒歩:<input type="radio" value="walk" name="transport" checked={transport === 'walk'} onChange={handleChange} />
-        </inputLabel>
+        <input type="radio" value="car" name="transport" checked={transport === 'car'} onChange={handleChange} id="transport-car" />
+        <label htmlFor="transport-car">車</label>
+        <input type="radio" value="walk" name="transport" checked={transport === 'walk'} onChange={handleChange} id="transport-walk" />
+        <label htmlFor="transport-walk">徒歩</label>
       </dd>
       <dt>有料道路、高速道路</dt>
       <dd>
-        <inputLabel>
-          <input type="checkbox" value="false" name="expressway" checked={expressway} onChange={handleChange} /> 利用する
-        </inputLabel>
+        <input type="checkbox" value="false" name="expressway" checked={expressway} onChange={handleChange} id="expressway" />
+        <label htmlFor="expressway">利用する</label>
       </dd>
       <dt>交通量の見積もり</dt>
       <dd>
-        <inputLabel>
-          通常: <input type="radio" value="standard" name="traffic" checked={traffic === 'standard'} onChange={handleChange} />
-        </inputLabel>
-        <inputLabel>
-          楽観的: <input type="radio" value="optimistic" name="traffic" checked={traffic === 'optimistic'} onChange={handleChange} />
-        </inputLabel>
-        <inputLabel>
-          悲観的: <input type="radio" value="pessimistic" name="traffic" checked={traffic === 'pessimistic'} onChange={handleChange} />
-        </inputLabel>
+        <input type="radio" value="standard" name="traffic" checked={traffic === 'standard'} onChange={handleChange} id="traffic-standard" />
+        <label htmlFor="traffic-standard">通常</label>
+        <input type="radio" value="optimistic" name="traffic" checked={traffic === 'optimistic'} onChange={handleChange} id="traffic-optimistic" />
+        <label htmlFor="traffic-optimistic">楽観的</label>
+        <input type="radio" value="pessimistic" name="traffic" checked={traffic === 'pessimistic'} onChange={handleChange} id="traffic-pessimistic" />
+        <label htmlFor="traffic-pessimistic">悲観的</label>
       </dd>
     </dl>
   );
+};
+
+DemoQuestionOptions.propTypes = {
+  transport: PropTypes.string.isRequired,
+  expressway: PropTypes.bool.isRequired,
+  traffic: PropTypes.string.isRequired,
+  handleFormChange: PropTypes.func.isRequired,
 };
 
 export default DemoQuestionOptions;
