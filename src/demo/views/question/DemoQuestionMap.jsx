57d01ffe447c896b7445d651429990570da40cae
maechabin <mail@chab.in>
2017-09-13 16:07:31 +0900

    マーカークリック時にルートを表示させる機能を実装

--- src/demo/views/question/DemoQuestionMap.jsx
+++ src/demo/views/question/DemoQuestionMap.jsx
@@ -1,66 +1,62 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class DemoQuestionMap extends React.Component {
-
-  componentDidMount() {
-    this.displayMap();
-  }
-
-  getGeocode(address) {
-    // ジオコーディング用インスタンスを生成
-    const geocoder = new this.props.google.maps.Geocoder();
-    const geocoderStatusOK = this.props.google.maps.GeocoderStatus.OK
-    // 与えた住所から位置情報を取得する関数
-    geocoder.geocode({ address }, (results, status) => {
-      if (status === geocoderStatusOK) {
-        return results[0].geometry.location;
-      }
-      return `Geocode was not successful for the following reason: ${status}`;
-    });
-  }
-
-  displayMap() {
-    const _this = this;
-    const bounds = new this.props.google.maps.LatLngBounds();
-    let marker;
-
-    // 地図を表示する際のオプションを設定
-    const mapOptions = {
+  constructor(props) {
+    super(props);
+    // 地図を表示する際のオプション
+    this.mapOptions = {
       center: new this.props.google.maps.LatLng(this.props.departure.lat, this.props.departure.lng),
       zoom: 12,
-      mapTypeId: this.props.google.maps.MapTypeId.ROADMAP
+      mapTypeId: this.props.google.maps.MapTypeId.ROADMAP,
     };
+    this.map = '';
+    // Google Maps API本体
+    this.gm = this.props.google.maps;
+  }
 
-    // Mapオブジェクトに地図表示要素情報とオプション情報を渡し、インスタンス生成
-    const map = new this.props.google.maps.Map(this.refs.DemoQuestionMap, mapOptions);
+  componentDidMount() {
+    this.map = new this.gm.Map(this.refs.DemoQuestionMap, this.mapOptions);
+    this.displayMarker();
+  }
 
-    marker = new this.props.google.maps.Marker({
+  componentDidUpdate() {
+    // this.map = new this.gm.Map(this.refs.DemoQuestionMap, this.mapOptions);
+    this.renderRoute();
+    this.displayMarker();
+  }
+
+  // propsから出発地、到着地、経路を取得して、地図にマーカーを表示するメソッド
+  displayMarker() {
+    let marker;
+    const bounds = new this.gm.LatLngBounds();
+
+    marker = new this.gm.Marker({
       position: { lat: this.props.departure.lat, lng: this.props.departure.lng },
-      map,
+      map: this.map,
       title: this.props.departure.title,
       label: this.props.departure.label,
     });
     bounds.extend(marker.position);
 
-    marker = new this.props.google.maps.Marker({
+    marker = new this.gm.Marker({
       position: { lat: this.props.arival.lat, lng: this.props.arival.lng },
-      map,
+      map: this.map,
       title: this.props.arival.title,
       label: this.props.arival.label,
     });
     bounds.extend(marker.position);
 
     this.props.routes.map((route) => {
-      marker = new this.props.google.maps.Marker({
+      marker = new this.gm.Marker({
         position: { lat: route.lat, lng: route.lng },
-        map,
+        map: this.map,
         title: route.title,
         icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
         label: route.label,
       });
       bounds.extend(marker.position);
       marker.addListener('click', () => {
-        console.log(route);
         if (route.sortId === 0) {
           return this.props.handleMarkerClick(route.id);
         }
@@ -68,7 +64,42 @@ class DemoQuestionMap extends React.Component {
       });
       return marker;
     });
-    map.fitBounds(bounds);
+    this.map.fitBounds(bounds);
+  }
+
+  // propsから出発地、到着地、経路、オプションを取得して、マーカーを結ぶ線（polyline）を地図に表示するメソッド
+  renderRoute() {
+    const directionsService = new this.gm.DirectionsService();
+    const directionsRenderer = new this.gm.DirectionsRenderer();
+    const filteredRoutes = this.props.routes.filter(route => route.sortId !== 0);
+    const wayPoints = filteredRoutes.map(route => ({ location: route.title }));
+    const destination = filteredRoutes.length === this.props.routes.length ? this.props.arival.title : filteredRoutes[filteredRoutes.length - 1];
+
+    // polylineをレンダリングする際のオプション
+    directionsRenderer.setOptions({
+      suppressMarkers: true, // マーカーを非表示にする場合はtrue
+      preserveViewport: true, // zoomさせないようにする場合はtrue
+      polylineOptions: {
+        strokeOpacity: 1.0, // polylineの透明度0.0~1.0
+        strokeWeight: 6, // polylineの幅（ピクセル）
+        strokeColor: '#6991fd', // polylineの色
+      },
+    });
+    // directions apiへのリクエスト
+    directionsService.route({
+      origin: this.props.departure.title, // 出発地
+      waypoints: wayPoints, // 経路（配列）
+      destination, // 到着地
+      travelMode: this.props.transport === 'car' ? this.gm.TravelMode.DRIVING : this.gm.TravelMode.WALKING, // 車(DRIVING) or 徒歩(WALKING)
+      avoidHighways: this.props.expressway !== 'no', // 高速は利用しない場合はfalse
+      optimizeWaypoints: false, // 最適化を有効にする場合はtrue
+    }, (response, status) => {
+      console.log(response);
+      console.log(status);
+      directionsRenderer.setDirections(response);
+    });
+    // polylineを地図に表示
+    directionsRenderer.setMap(this.map);
   }
 
   render() {
@@ -79,7 +110,19 @@ class DemoQuestionMap extends React.Component {
 }
 
 DemoQuestionMap.propTypes = {
-
+  departure: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    lat: PropTypes.number.isRequired,
+    lng: PropTypes.number.isRequired,
+    label: PropTypes.string.isRequired,
+  }).isRequired,
+  arival: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    lat: PropTypes.number.isRequired,
+    lng: PropTypes.number.isRequired,
+    label: PropTypes.string.isRequired,
+  }).isRequired,
+  handleMarkerClick: PropTypes.func.isRequired,
 };
 
 export default DemoQuestionMap;
