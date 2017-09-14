f9a7630ed1a97f0edb154726843157aec010ddce
maechabin <mail@chab.in>
2017-09-14 10:18:49 +0900

    表示するマーカーの数を8つに増やした

--- src/demo/demoState.js
+++ src/demo/demoState.js
@@ -7,9 +7,12 @@ const demoState = {
     { id: 3, title: '明治大学', lat: 35.6972422, lng: 139.7615346, label: '経路C', sortId: 0 },
     { id: 4, title: '浜離宮', lat: 35.6597374, lng: 139.76349249999998, label: '経路D', sortId: 0 },
     { id: 5, title: '国会議事堂', lat: 35.675888, lng: 139.74485800000002, label: '経路E', sortId: 0 },
+    { id: 6, title: '東京タワー', lat: 35.6585805, lng: 139.745432899999977, label: '経路F', sortId: 0 },
+    { id: 7, title: '上野動物園', lat: 35.7164535, lng: 139.77131770000005, label: '経路G', sortId: 0 },
+    { id: 8, title: '東京駅', lat: 35.6811673, lng: 139.76705160000006, label: '経路H', sortId: 0 },
   ],
   choosingRouteFlag: false,
-  currentSortId: 5,
+  currentSortId: 8, // routesの要素の数
   transport: 'car',
   expressway: 'no',
   traffic: 'standard',
