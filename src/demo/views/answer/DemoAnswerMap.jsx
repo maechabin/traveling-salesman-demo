e7020ffe76b8af951ca8b6f77214fafb115d2a6e
maechabin <mail@chab.in>
2017-09-15 14:51:36 +0900

    DEMO-45 最短ルートを地図に表示する

--- src/demo/views/answer/DemoAnswerMap.jsx
+++ src/demo/views/answer/DemoAnswerMap.jsx
@@ -12,7 +12,7 @@ class DemoAnswerMap extends React.PureComponent {
     };
     this.map = '';
     // Google Maps API本体
-    this.gm = this.props.google.maps;   
+    this.gm = this.props.google.maps;
   }
 
   componentDidMount() {
@@ -23,6 +23,7 @@ class DemoAnswerMap extends React.PureComponent {
   init() {
     this.map = new this.gm.Map(this.refs.DemoAnswerMap, this.mapOptions);
     this.displayMarker();
+    this.renderRoute();
   }
 
   // propsから出発地、到着地、経路を取得して、地図にマーカーを表示するメソッド
@@ -62,6 +63,59 @@ class DemoAnswerMap extends React.PureComponent {
       return marker;
     });
     this.map.fitBounds(bounds);
+  }
+
+  // propsから出発地、到着地、経路、オプションを取得して、マーカーを結ぶ線（polyline）を地図に表示するメソッド
+  renderRoute() {
+    const directionsService = new this.gm.DirectionsService();
+    const directionsRenderer = new this.gm.DirectionsRenderer();
+    const wayPoints = this.props.routes.map(route => ({ location: route.title }));
+
+    // polylineをレンダリングする際のオプション
+    directionsRenderer.setOptions({
+      suppressMarkers: true, // マーカーを非表示にする場合はtrue
+      preserveViewport: true, // zoomさせないようにする場合はtrue
+      polylineOptions: {
+        strokeOpacity: 1.0, // polylineの透明度0.0~1.0
+        strokeWeight: 6, // polylineの幅（ピクセル）
+        strokeColor: '#6991fd', // polylineの色
+      },
+    });
+    // directions apiへのリクエスト
+    directionsService.route({
+      origin: this.props.departure.title, // 出発地
+      waypoints: wayPoints, // 経路（配列）
+      destination: this.props.arival.title, // 到着地
+      travelMode: this.props.transport === 'car' ? this.gm.TravelMode.DRIVING : this.gm.TravelMode.WALKING, // 車(DRIVING) or 徒歩(WALKING)
+      avoidHighways: this.props.expressway !== 'no', // 高速は利用しない場合はfalse
+      optimizeWaypoints: true, // 最適化を有効にする場合はtrue
+      drivingOptions: {
+        departureTime: new Date(),
+        trafficModel: this.props.traffic === 'standard' ? this.gm.TrafficModel.BEST_GUESS : this.props.traffic === 'optimistic' ? this.gm.TrafficModel.OPTIMISTIC : this.gm.TrafficModel.PESSIMISTIC,
+      },
+    }, (response, status) => {
+      if (status === this.gm.DirectionsStatus.OK) {
+        // directions apiのレスポンスをセット
+        directionsRenderer.setDirections(response);
+
+        // 総距離、総時間を表示
+        let gross = {};
+        let distance = 0;
+        let duration = 0;
+        response.routes[0].legs.forEach((item) => {
+          distance += item.distance.value;
+          duration += item.duration.value;
+          gross = {
+            distance: Math.floor((distance / 1000) * (10 ** 1)) / (10 ** 1), // 小数点第1位以下を切り捨て
+            duration: Math.floor((duration / 60) * (10 ** 1)) / (10 ** 1), // // 小数点第1位以下を切り捨て
+          };
+        });
+        return this.props.handleUpdateGross(gross);
+      }
+      return `error: ${status}`;
+    });
+    // polylineを地図に表示
+    directionsRenderer.setMap(this.map);
   }
 
   render() {
