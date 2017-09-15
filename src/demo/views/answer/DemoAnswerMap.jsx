2c397d631e49ee231cf1cc052ebeca6192431b83
maechabin <mail@chab.in>
2017-09-15 14:20:02 +0900

    DEMO-43 地図を表示させる

--- src/demo/views/answer/DemoAnswerMap.jsx
+++ src/demo/views/answer/DemoAnswerMap.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const DemoAnswerMap = (props) => {
-  return (
-    <div className="DemoAnswerMap">DemoAnswerMap</div>
-  );
-};
+class DemoAnswerMap extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    // 地図を表示する際のオプション
+    this.mapOptions = {
+      center: new this.props.google.maps.LatLng(this.props.departure.lat, this.props.departure.lng),
+      zoom: 12,
+      mapTypeId: this.props.google.maps.MapTypeId.ROADMAP,
+    };
+    this.map = '';
+    // Google Maps API本体
+    this.gm = this.props.google.maps;   
+  }
+
+  componentDidMount() {
+    this.init();
+  }
+
+  // 初期化
+  init() {
+    this.map = new this.gm.Map(this.refs.DemoAnswerMap, this.mapOptions);
+    // this.displayMarker();
+    this.props.handleInit();
+  }
+
+  render() {
+    return (
+      <div ref="DemoAnswerMap" className="DemoAnswerMap">DemoAnswerMap</div>
+    );
+  }
+}
 
 DemoAnswerMap.propTypes = {
 
