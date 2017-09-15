5b3e92066ccce0b2b5291e344a3a33362551981a
maechabin <mail@chab.in>
2017-09-15 11:41:33 +0900

    DEMO-35 回答を見るボタンを押したら正解画面に切り替わるようにする

--- src/demo/views/Demo.jsx
+++ src/demo/views/Demo.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import DemoHeader from './DemoHeader';
 import DemoQuestion from './question/DemoQuestion';
 import DemoDescription from './description/DemoDescription';
+import DemoAnswer from './answer/DemoAnswer';
+
 import '../styles/Demo.css';
 
-const Demo = props => (
-  <div className="Demo">
-    <DemoHeader />
-    <main className="DemoMain">
-      <DemoQuestion {...props} />
-      <DemoDescription />
-    </main>
-  </div>
-);
+const Demo = (props) => {
+  const { google, ...rest } = props;
+  const rightComponent = props.viewAnswerFlag ? <DemoAnswer {...props} /> : <DemoDescription {...rest} />;
+
+  return (
+    <div className="Demo">
+      <DemoHeader />
+      <main className="DemoMain">
+        <DemoQuestion {...props} />
+        {rightComponent}
+      </main>
+    </div>
+  );
+};
+
+Demo.propTypes = {
+  google: PropTypes.object.isRequired,
+  viewAnswerFlag: PropTypes.bool.isRequired,
+};
 
 export default Demo;
