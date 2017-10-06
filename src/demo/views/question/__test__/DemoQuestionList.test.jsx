e830c7dc35a2936b6a55ff569943ff8161e10122
maechabin <mail@chab.in>
2017-10-06 17:39:33 +0900

    経路リスト変更

--- src/demo/views/question/__test__/DemoQuestionList.test.jsx
+++ src/demo/views/question/__test__/DemoQuestionList.test.jsx
@@ -23,7 +23,7 @@ describe('<DemoQuestionList />', () => {
       <DemoQuestionList
         routes={list}
         departure={departure}
-        arival={arival} 
+        arival={arival}
         answerWaypointOrder={answerWaypointOrder}
       />,
     );
@@ -121,5 +121,57 @@ describe('<DemoQuestionList />', () => {
       />,
     );
     expect(wrapper.find('.DemoQuestionListRoutes').text()).toBe('経路E:国会議事堂経路G:上野動物園経路H:東京駅経路A:秋葉原駅経路B:文京区役所経路C:明治大学経路D:浜離宮経路F:東京タワー');
+  });
+
+  it('answerWaypointOrderの要素数が0であれば、class名にDemoQuestionListMistakeを持ったリストはないこと', () => {
+    const list = [
+      { id: 1, title: '秋葉原駅', lat: 35.698353, lng: 139.77311429999997, label: '経路A', sortId: 0 },
+      { id: 2, title: '文京区役所', lat: 35.7080677, lng: 139.752167, label: '経路B', sortId: 0 },
+      { id: 3, title: '明治大学', lat: 35.6972422, lng: 139.7615346, label: '経路C', sortId: 0 },
+      { id: 4, title: '浜離宮', lat: 35.6597374, lng: 139.76349249999998, label: '経路D', sortId: 0 },
+      { id: 5, title: '国会議事堂', lat: 35.675888, lng: 139.74485800000002, label: '経路E', sortId: 8 },
+      { id: 6, title: '東京タワー', lat: 35.6585805, lng: 139.745432899999977, label: '経路F', sortId: 0 },
+      { id: 7, title: '上野動物園', lat: 35.7164535, lng: 139.77131770000005, label: '経路G', sortId: 7 },
+      { id: 8, title: '東京駅', lat: 35.6811673, lng: 139.76705160000006, label: '経路H', sortId: 6 },
+    ];
+    const answerWaypointOrder = [];
+    const wrapper = shallow(
+      <DemoQuestionList
+        routes={list}
+        departure={departure}
+        arival={arival}
+        answerWaypointOrder={answerWaypointOrder}
+      />,
+    );
+    const classNames = wrapper.find('.DemoQuestionListRoutes').props().children.map(li => li.props.className);
+    expect(classNames).not.toEqual('DemoQuestionListSorted DemoQuestionListMistake');
+    expect(classNames).not.toEqual('DemoQuestionListNotSorted DemoQuestionListMistake');
+    expect(classNames).not.toEqual('DemoQuestionListMistake');
+  });
+
+  it('正解の経路リストと一致しなかった場合は、class名にDemoQuestionListMistakeが付与されること', () => {
+    const list = [
+      { id: 1, title: '秋葉原駅', lat: 35.698353, lng: 139.77311429999997, label: '経路A', sortId: 8 },
+      { id: 2, title: '文京区役所', lat: 35.7080677, lng: 139.752167, label: '経路B', sortId: 4 },
+      { id: 3, title: '明治大学', lat: 35.6972422, lng: 139.7615346, label: '経路C', sortId: 7 },
+      { id: 4, title: '浜離宮', lat: 35.6597374, lng: 139.76349249999998, label: '経路D', sortId: 1 },
+      { id: 5, title: '国会議事堂', lat: 35.675888, lng: 139.74485800000002, label: '経路E', sortId: 5 },
+      { id: 6, title: '東京タワー', lat: 35.6585805, lng: 139.745432899999977, label: '経路F', sortId: 2 },
+      { id: 7, title: '上野動物園', lat: 35.7164535, lng: 139.77131770000005, label: '経路G', sortId: 3 },
+      { id: 8, title: '東京駅', lat: 35.6811673, lng: 139.76705160000006, label: '経路H', sortId: 6 },
+    ];
+    const answerWaypointOrder = [4, 5, 0, 1, 2, 3, 6, 7];
+    const wrapper = shallow(
+      <DemoQuestionList
+        routes={list}
+        departure={departure}
+        arival={arival}
+        answerWaypointOrder={answerWaypointOrder}
+      />,
+    );
+    const mistakedList = wrapper.find('.DemoQuestionListRoutes').props().children.filter(
+      (li, i) => answerWaypointOrder[i] !== i,
+    );
+    mistakedList.map(li => expect(li.props.className).toEqual('DemoQuestionListSorted DemoQuestionListMistake'));
   });
 });
