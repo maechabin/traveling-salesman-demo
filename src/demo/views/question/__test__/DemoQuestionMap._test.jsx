a6aeef5d40ce24984b927803458eb665191ccf19
maechabin <mail@chab.in>
2019-03-15 22:44:47 +0900

    昔のコミットもれ

---
+++ src/demo/views/question/__test__/DemoQuestionMap._test.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import sinon from 'sinon';
+import { shallow, mount } from 'enzyme';
+
+import DemoQuestionMap from '../DemoQuestionMap';
+import demoState from '../../../demoState';
+
+describe('<DemoQuestionMap />', () => {
+  const restProps = {
+    google: {
+      maps: {
+        LatLngBounds: function () {
+          return {
+            extend: jest.fn(),
+          };
+        },
+        LatLng: jest.fn(),
+        MapTypeId: {
+          ROADMAP: '',
+        },
+        Map: jest.fn(),
+        Marker: function () {
+          addListener: jest.fn(),
+        },
+      },
+    },
+    handleMarkerClick: jest.fn(),
+    handleUpdateGross: jest.fn(),
+    handleInit: jest.fn(),
+  };
+  it('componentDidMountを呼び出すこと', () => {
+    sinon.spy(DemoQuestionMap.prototype, 'componentDidMount');
+    const wrapper = mount(
+      <DemoQuestionMap
+        {...demoState}
+        {...restProps}
+      />,
+    );
+    expect(DemoQuestionMap.prototype.componentDidMount.callOnce).toEqual(true);
+  });
+});
+
