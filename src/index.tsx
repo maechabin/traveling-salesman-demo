679d8c4b660643551d73bf5dfd3be5f641193895
maechabin <mitoppo@gmail.com>
2019-03-16 16:27:09 +0900

    index.jsをtsx化

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import { applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
 import configureStore from './store';
 import demoReducer from './demo/demoReducer';
-import demoState from './demo/demoState';
+import state from './state';
 
 import './index.css';
 import DemoContainer from './demo/DemoContainer';
+import registerServiceWorker from './registerServiceWorker';
 
 const middleware = () => applyMiddleware(thunk);
 
-const store = configureStore(demoReducer, demoState, middleware());
+const store = configureStore(demoReducer, state, middleware());
 
 ReactDOM.render(
   <Provider store={store}>
-    <DemoContainer google={window.google} />
+    <DemoContainer />
   </Provider>,
   document.getElementById('root'),
 );
+
+registerServiceWorker();
