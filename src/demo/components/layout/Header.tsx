41bd575e7abdd9266b169c3570218f04b1bd10ba
maechabin <mitoppo@gmail.com>
2019-03-18 00:53:36 +0900

    style調整

--- src/demo/components/layout/Header.tsx
+++ src/demo/components/layout/Header.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
 function Header(): JSX.Element {
+  const style = {
+    header: {
+      margin: 0,
+      padding: 0,
+      lineHeight: '40px',
+      textAlign: 'center',
+      backgroundColor: '#7fbc39',
+      color: '#fff',
+    } as React.CSSProperties,
+    h1: {
+      margin: 0,
+      padding: 0,
+      fontSize: '16px',
+    } as React.CSSProperties,
+  };
+
   return (
-    <header className="DemoHeader">
-      <h1>サラリーマン巡回問題 ~最短経路を探せ！~</h1>
+    <header style={style.header}>
+      <h1 style={style.h1}>サラリーマン巡回問題 ~最短経路を探せ！~</h1>
     </header>
   );
 }
