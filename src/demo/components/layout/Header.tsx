ed4b6bc35511c19016695133bf6f611e02cd477b
maechabin <mitoppo@gmail.com>
2019-03-30 17:37:51 +0900

    githubへのリンク追加

--- src/demo/components/layout/Header.tsx
+++ src/demo/components/layout/Header.tsx
@@ -9,17 +9,36 @@ function Header(): JSX.Element {
       textAlign: 'center',
       backgroundColor: '#7fbc39',
       color: '#fff',
+      position: 'relative',
     } as React.CSSProperties,
     h1: {
       margin: 0,
       padding: 0,
       fontSize: '16px',
     } as React.CSSProperties,
+    github: {
+      margin: 0,
+      padding: 0,
+      position: 'absolute',
+      top: 0,
+      right: '16px',
+    } as React.CSSProperties,
+    link: {
+      color: '#fff',
+    },
   };
 
   return (
     <header style={style.header}>
       <h1 style={style.h1}>サラリーマン巡回問題 ~最短経路を探せ！~</h1>
+      <p style={style.github}>
+        <a
+          style={style.link}
+          href="https://github.com/maechabin/traveling-salesman-demo"
+          target="_blank">
+          GitHub
+        </a>
+      </p>
     </header>
   );
 }
