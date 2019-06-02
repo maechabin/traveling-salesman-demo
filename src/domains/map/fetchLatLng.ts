d3b12524a967c975fd888439ee62f9f483c7e659
maechabin <mitoppo@gmail.com>
2019-06-03 02:44:02 +0900

    リファクタ

--- src/domains/map/fetchLatLng.ts
+++ src/domains/map/fetchLatLng.ts
@@ -1,7 +1,16 @@
-export async function fetchLatLng(address: string): Promise<{ lat: number; lng: number }> {
-  const res = await fetch(
-    `https://us-central1-maps-functions-6b26b.cloudfunctions.net/geocoding?address=${address}`,
-  );
-  const latlng = await res.json();
-  return latlng.results[0].geometry.location;
+export interface LatLng {
+  lat: number;
+  lng: number;
+}
+
+export async function fetchLatLng(address: string): Promise<LatLng> {
+  try {
+    const res = await fetch(
+      `${process.env.REACT_APP_GOOGLE_MAPS_API_URI}/geocoding?address=${address}`,
+    );
+    const latlng = await res.json();
+    return latlng.results[0].geometry.location;
+  } catch (error) {
+    return error;
+  }
 }
